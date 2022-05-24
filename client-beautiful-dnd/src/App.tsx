d15e6a67cc269680f668efb73e509fc1e0253b0c
Mathieu Tinoco <tinoco.mathieu@gmail.com>
2022-05-24 22:21:35 +0200

    added drag and drop logic and styling but component not dropping

--- client-beautiful-dnd/src/App.tsx
+++ client-beautiful-dnd/src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { useState, useRef, useEffect } from 'react';
 import List from "./components/List";
-import { DragDropContext } from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 
 export interface taskType {
   id: number,
@@ -11,7 +11,7 @@ export interface taskType {
 }
 
 function App() {
-  const [taskList, setTaskList] = useState<taskType[]>([{id:Date.now(), task:'hello', isAccomplished: false}]);
+  const [taskList, setTaskList] = useState<taskType[]>([]); //{id:Date.now(), task:'hello', isAccomplished: false}
   const [completedTodos, setCompletedTodos] = useState<taskType[]>([])
   const creationField = useRef<HTMLInputElement | null>(null);
 
@@ -24,8 +24,44 @@ console.log(taskList)
     setTaskList(prev => [...prev, {id: Date.now(),task: creationField.current!.value, isAccomplished: false}]);
   }
 
+  function onDragEnd (result: DropResult) {
+    const { source, destination } = result;
+    console.log(source, destination)
+    if (!destination) {
+      return
+    }
+    if (destination.droppableId === source.droppableId && destination.index === source.index) {
+      return
+    }
+    let add, active = [...taskList], complete = [...completedTodos];
+    if (source.droppableId === "Todo") {
+      console.log('here')
+      console.log('source.index', source.index)
+      add = active[source.index];
+      console.log('add', add)
+      active.splice(source.index, 1);
+      console.log('active should be empty', active)
+    } else {
+      console.log("not here 1")
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+    if (destination.droppableId === "Accomplished") {
+      console.log("here")
+      active.splice(destination.index, 0, add);
+      console.log('active add', active)
+    } else {
+      console.log("not here 2")
+      complete.splice(destination.index, 0, add);
+    }
+    console.log(complete)
+    console.log('active', active)
+    setCompletedTodos(complete);
+    setTaskList(active);
+  }
+
   return (
-    <DragDropContext onDragEnd={() => { }}>
+    <DragDropContext onDragEnd={onDragEnd}>
       <div className='flex-col space-y-5 min-h-screen bg-gradient-to-r from-color1 to-color2'>
         <h1 className='font-bold text-center py-4 text-2xl' >Task Manager</h1>
         <div className="text-center">
@@ -41,10 +77,10 @@ console.log(taskList)
             setList={setTaskList}
 
           />
-          {/* <List
+          <List
             title={"Accomplished"}
             list={completedTodos}
-            setList={setCompletedTodos} /> */}
+            setList={setCompletedTodos} />
         </div>
 
       </div>
--- client-beautiful-dnd/src/components/List.tsx
+++ client-beautiful-dnd/src/components/List.tsx
@@ -4,24 +4,30 @@ import '../styles/List.css';
 import { Droppable } from 'react-beautiful-dnd';
 import { taskType } from '../App';
 
-export default function List({ title, list, setList }: { title: string, list: taskType[], setList:React.Dispatch<React.SetStateAction<taskType[]>> }) {
+export default function List({ title, list, setList }: { title: string, list: taskType[], setList: React.Dispatch<React.SetStateAction<taskType[]>> }) {
   return (
-    <div className='w-full max-w-xs bg-slate-400 rounded-xl p-4' >
+    <>
       <Droppable droppableId={title} >
         {
-          (provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps} >
-              <h2 className='pl-2 mb-2 font-bold' >{title}</h2>
-              <div className='flex-column space-y-2' >
-                {list.map((task, index) => {
-                  return <SingleTask task={task} key={task.id} index={index} setList={setList}/>
-                })}
+          (provided, snapshot) => (
+              <div ref={provided.innerRef} {...provided.droppableProps} className={`${snapshot.isDraggingOver && 'bg-slate-500'} w-full max-w-xs bg-slate-400 rounded-xl p-4 h-fit`}>
+                <h2 className='pl-2 mb-2 font-bold' >{title}</h2>
+                <div className='flex-column space-y-2' >
 
+                  {list.map((task, index) => (
+                    <SingleTask
+                      task={task}
+                      key={task.id}
+                      index={index}
+                      setList={setList}
+                    />
+                  ))}
+                </div>
+                {provided.placeholder}
               </div>
-            </div>
           )
         }
       </Droppable>
-    </div>
+    </>
   )
 }
